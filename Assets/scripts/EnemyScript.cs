4d7b280026b10da6ba15e3401c11fb5b47c1d950
Radu Birsan <radu_birsan@hotmail.com>
2017-03-08 14:35:12 -0500

    added the game

---
+++ Assets/scripts/EnemyScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+	private WeaponScript[] weapons;
+	private bool hasSpawn;
+	private MoveScript moveScript;
+	private Collider2D coliderComponent;
+	private SpriteRenderer rendererComponent;
+
+	void Awake () {
+		weapons = GetComponentsInChildren<WeaponScript> ();
+		moveScript = GetComponent<MoveScript> ();
+		coliderComponent = GetComponent<Collider2D> ();
+		rendererComponent = GetComponent<SpriteRenderer> ();
+	}
+	// Use this for initialization
+	void Start () {
+		hasSpawn = false;
+		coliderComponent.enabled = false;
+		//moveScript.enabled = false;
+		foreach (WeaponScript weapon in weapons) {
+			weapon.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hasSpawn == false) {
+			if (rendererComponent.IsVisibleFrom (Camera.main)) {
+				Spawn ();
+			}
+		} else {
+			foreach (WeaponScript weapon in weapons) {
+				if (weapon != null && weapon.enabled && weapon.CanAttack) {
+					weapon.Attack (true);
+				}
+			}
+			if (rendererComponent.IsVisibleFrom (Camera.main) == false) {
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	private void Spawn() {
+		hasSpawn = true;
+		coliderComponent.enabled = true;
+		//moveScript.enabled = true;
+		foreach (WeaponScript weapon in weapons) {
+			weapon.enabled = true;
+		}
+	}
+}
---
+++ Assets/scripts/ScrollingScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingScript : MonoBehaviour {
+	public Vector2 speed = new Vector2 (10, 10);
+	public Vector2 direction = new Vector2 (-1, 0);
+	public bool isLinkedToCamera = false;
+	public bool isLooping = false;
+	private List<SpriteRenderer> backgroundPart;
+	// Use this for initialization
+	void Start () {
+		if (isLooping) {
+			backgroundPart = new List<SpriteRenderer> ();
+			for (int i = 0; i < transform.childCount; i++) {
+				Transform child = transform.GetChild (i);
+				SpriteRenderer r = child.GetComponent<SpriteRenderer> ();
+				if (r != null) {
+					backgroundPart.Add (r);
+				}
+			}
+			backgroundPart = backgroundPart.OrderBy (
+				t => t.transform.position.x
+			).ToList ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3 (
+			                   speed.x * direction.x,
+			                   speed.y * direction.y,
+			                   0);
+		movement *= Time.deltaTime;
+		transform.Translate (movement);
+		if (isLinkedToCamera) {
+			Camera.main.transform.Translate (movement);
+		}
+		if (isLooping) {
+			SpriteRenderer firstChild = backgroundPart.FirstOrDefault ();
+			if (firstChild != null) {
+				if (firstChild.transform.position.x < Camera.main.transform.position.x) {
+					if (firstChild.IsVisibleFrom (Camera.main) == false) {
+						SpriteRenderer lastChild = backgroundPart.LastOrDefault ();
+						Vector3 lastPosition = lastChild.transform.position;
+						Vector3 lastSize = (lastChild.bounds.max - lastChild.bounds.min);
+						firstChild.transform.position = new
+							Vector3 (lastPosition.x + lastSize.x, firstChild.transform.position.y, firstChild.transform.position.z);
+						backgroundPart.Remove (firstChild);
+						backgroundPart.Add (firstChild);
+					}
+				}
+			}
+		}
+	}
+}
